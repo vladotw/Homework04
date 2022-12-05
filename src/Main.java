28d1b36052e1aaf12e8393a1efc7014350754c4c
Vlad <vladot@ya.ru>
2022-12-05 13:51:38 +0600

    Task1, Task2

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,46 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
 
     }
-        public static void task1() {
-            System.out.println("Задача 1");
-            System.out.println();
+    public static void task1() {
+        System.out.println("Задача 1");
+        System.out.println();
 
-            int age = 10;
+        int age = 10;
 
-            if (age >= 18) {
-                System.out.println("Вам 18 или более лет");
-            }
-            if (age < 18) {
-                System.out.println("Вам меньше 18 лет");
-            }
-
+        if (age >= 18) {
+            System.out.println("Вам 18 или более лет");
         }
+        if (age < 18) {
+            System.out.println("Вам меньше 18 лет");
+        }
+
+        System.out.println();
+        System.out.println();
+    }
+
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+        System.out.println();
+
+        int outdoorTemperature = -3;
+
+        if (outdoorTemperature < 5) {
+            System.out.println("На улице " + outdoorTemperature + " градусов, нужно надеть " +
+                    "шапку");
+        }
+
+        if (outdoorTemperature > 5) {
+            System.out.println("На улице " + outdoorTemperature + " градусов, можно идти " +
+                    "без шапки");
+        }
+
+        System.out.println();
+        System.out.println();
+    }
 
 
 
