404f02dc70668ded995ce72a08f6f2e20f6711c6
Vlad <vladot@ya.ru>
2022-12-05 16:14:28 +0600

    Task1, Task2, Task3, Task4, Task5, Task6, Task7

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
         task2();
         task3();
         task4();
-
+        task5();
+        task6();
+        task7();
 
     }
     public static void task1() {
@@ -92,5 +94,79 @@ public class Main {
         System.out.println();
     }
 
+    public static void task5() {
+        System.out.println("Задача 5");
+        System.out.println();
+        int age = 15;
+        boolean canRideWithAdult = age >= 5 && age < 14;
 
+        if(age < 5) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему нельзя кататься на " +
+                    "аттракционе");
+        }
+        else if (canRideWithAdult) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на " +
+                    "аттракционе в сопровождении  взрослого");
+        }
+        else if (age >= 14) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на " +
+                    "аттракционе без сопровождения взрослого");
+
+        }
+        System.out.println();
+        System.out.println();
+
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+        System.out.println();
+
+        int carriageCapacity = 102;
+        int numberOfSittingPlaces = 60;
+
+        int numberOfPassengers = 105;
+
+        boolean sittingPlacesLeft = numberOfPassengers < numberOfSittingPlaces;
+        boolean standingPlacesLeft = numberOfPassengers >= numberOfSittingPlaces &&
+                numberOfPassengers < carriageCapacity;
+        boolean noPlacesMore = numberOfPassengers == carriageCapacity;
+        boolean carriageCrowded = numberOfPassengers > carriageCapacity;
+
+        if (sittingPlacesLeft) {
+            System.out.println("В вагоне ещё есть сидячие места");
+        }
+        else if (standingPlacesLeft) {
+            System.out.println("В вагоне остались только стоячие места");
+        }
+        else if (noPlacesMore) {
+            System.out.println("В вагоне не осталось свободных мест");
+        }
+        else if (carriageCrowded) {
+            System.out.println("Вагон переполнен");
+        }
+
+
+        System.out.println();
+        System.out.println();
+
+    }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+        System.out.println();
+        int one = 9;
+        int two = 10;
+        int three = 8;
+
+        if (one > two && one > three) {
+            System.out.println("Наиболльшее число " + one);
+        } else if (two > one && two > three) {
+            System.out.println("Наиболльшее число " + two);
+            }
+        else {
+            System.out.println("Наиболльшее число " + three);
+        }
+
+    }
 }
