5088c1d26538a6a7cddc95de85a2e988a5ce044f
Vlad <vladot@ya.ru>
2022-12-05 13:39:46 +0600

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
 
-        System.out.println();
     }
+        public static void task1() {
+            System.out.println("Задача 1");
+            System.out.println();
+
+            int age = 10;
+
+            if (age >= 18) {
+                System.out.println("Вам 18 или более лет");
+            }
+            else {
+                System.out.println("Вам меньше 18 лет");
+            }
+
+        }
+
+
+
 }
